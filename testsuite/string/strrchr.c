ed06dabdce3aa3b243f4d3e6e0cb301f3d5c4f35
Kai Tietz <ktietz70@googlemail.com>
2011-04-10 11:39:04 +0000

    Add strrchr testcase.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/experimental/ironcrate@4126 4407c894-4637-0410-b4f5-ada5f102cad1

---
+++ testsuite/string/strrchr.c
@@ -0,0 +1,42 @@
+#include "../testlib/main_test.c"
+#include <errno.h>
+
+/* char * __cdecl strrchr (const char *s, int ch).
+
+   Flow-graph:
+   (1): Search in s for first occurance of ch from right to left.
+   (2): Return found position in s of ch, otherwise NULL.
+
+Tests:
+	Test for search of character not in string.
+	Test for search of character 0.
+	Test for successful search of character != 0.
+	Test for s == NULL.
+*/
+
+int
+ic_test_main (int seq, int flags)
+{
+  const char *tst_str = "abc";
+  char * (*fct)(const char *, int) =
+	ic_test_getsymbol ("strrchr");
+  if (!fct)
+    return IC_TEST_RSLT_UNSUPPORTED;
+  switch (seq)
+  {
+    case 0:
+      ic_test_reset_internal_vars ();
+      IC_TEST_CHECK ((*fct)(tst_str, 'A') == NULL);
+      IC_TEST_CHECK ((*fct)(tst_str, 0) == (tst_str + strlen (tst_str)));
+      IC_TEST_CHECK ((*fct)(tst_str, 'b') == (tst_str + 1));
+      IC_TEST_CHECK ((*fct)(NULL, 0) == 0);
+      return IC_TEST_RSLT_FAILED;
+    case 1:
+      IC_TEST_CHECK (ic_test_segfault != 0);
+      return IC_TEST_RSLT_SUCCESS;
+    default:
+      break;
+  }
+  return IC_TEST_RSLT_FAILED;
+}
+
