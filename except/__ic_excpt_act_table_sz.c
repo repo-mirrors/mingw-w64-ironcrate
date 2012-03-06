d9dd36c77a45e5fb015f529a014c6ea8bb03b46b
Kai Tietz <ktietz70@googlemail.com>
2012-03-06 20:01:50 +0000

    Add exception action-table's total-size and element-count variables.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/experimental/ironcrate@4874 4407c894-4637-0410-b4f5-ada5f102cad1

---
+++ except/__ic_excpt_act_table_sz.c
@@ -0,0 +1,5 @@
+#include <ic_stdc.h>
+
+/* Keep in sync with __ic_excpt_act_table.  */
+int __ic_excpt_act_table_sz = sizeof (struct _ic_exception_action_t)
+			      * 10;
--- include/ic_gbls_int.h
+++ include/ic_gbls_int.h
@@ -2,6 +2,8 @@
 
 /* Exception-management.  */
 extern struct _ic_exception_action_t __ic_excpt_act_table[];
+extern int __ic_excpt_act_table_sz;
+extern int __ic_excpt_act_table_num;
 
 /* Memory-management.  */
 extern HANDLE _ic_theheap;
