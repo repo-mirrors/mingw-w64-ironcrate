54b86627854ca9621bacd41ca1239220e50efe08
Kai Tietz <ktietz70@googlemail.com>
2012-03-06 09:52:33 +0000

    Add more locale structures ...
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/experimental/ironcrate@4862 4407c894-4637-0410-b4f5-ada5f102cad1

--- include/ic_types.h
+++ include/ic_types.h
@@ -42,3 +42,54 @@ typedef struct _ic_setloc_t
   struct _ic_ctype_compat _Lcid_c[5];
 } _ic_setloc_t;
 
+typedef struct ic_tmbinfo_t
+{
+  int refcount;
+  int mbcodepage;
+  int ismbcodepage;
+  int mblcid;
+  unsigned short mbulinfo[6];
+  unsigned char mbctype[257];
+  unsigned char mbcasemap[256];
+} ic_tmbinfo_t;
+
+typedef struct ic_lc_timedate_t {
+  char *wday_abbr[7];
+  char *wday[7];
+  char *month_abbr[12];
+  char *month[12];
+  char *ampm[2];
+  char *ww_sdatefmt;
+  char *ww_ldatefmt;
+  char *ww_timefmt;
+  LCID ww_lcid;
+  int  ww_caltype;
+  int  refcount;
+} ic_lc_timedate_t;
+
+typedef struct ic_threadlocinfo_t {
+  int refcount;
+  unsigned int lc_codepage;
+  unsigned int lc_collate_cp;
+  unsigned long lc_handle[6]; /* LCID */
+  LC_ID lc_id[6];
+  struct {
+    char *locale;
+    wchar_t *wlocale;
+    int *refcount;
+    int *wrefcount;
+  } lc_category[6];
+  int lc_clike;
+  int mb_cur_max;
+  int *lconv_intl_refcount;
+  int *lconv_num_refcount;
+  int *lconv_mon_refcount;
+  struct ic_lconv_t *lconv;
+  int *ctype1_refcount;
+  unsigned short *ctype1;
+  const unsigned short *ic_pctype;
+  const unsigned char *pclmap;
+  const unsigned char *pcumap;
+  ic_lc_timedate_t *lc_time_curr;
+} ic_threadlocinfo_t;
+
