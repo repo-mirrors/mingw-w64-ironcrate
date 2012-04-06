142e63491bbf28e4b846cf9b5c15e961400492aa
Kai Tietz <ktietz70@googlemail.com>
2012-04-06 20:36:27 +0000

    Add test for _strset_s.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/experimental/ironcrate@4943 4407c894-4637-0410-b4f5-ada5f102cad1

---
+++ testsuite/string/_strset_s.c
@@ -0,0 +1,48 @@
+#include "../testlib/main_test.c"
+#include <errno.h>
+
+/* errno_t __cdecl _strset_s (char *s, size_t n, int ch).
+
+   Flow-graph:
+    (1) If s or n are zero, set errno to EINVAL and return EINVAL.
+    (2) Set string in s to value ch until not the terminating zero-
+	character of s is reached, or maximal n chacters are set.
+    (3) If the n'th chacter in s was set, then set *s to zero,
+	set errno to EINVAL, and return EINVAL.
+    (4) Otherwise return 0.
+Tests:
+	Test for s == NULL.
+	Test for valid s & n and ch != 0.
+	Test for valid s & n and ch == 0.
+	Test for valid s ch != 0, and n == strlen (s).
+*/
+
+int
+ic_test_main (int seq, int flags)
+{
+  char tst[] = "abc";
+  char ts2[] = "abc";
+  char ts3[] = "abcd";
+  errno_t (*fct)(char *, size_t, int) =
+	ic_test_getsymbol ("_strset_s");
+  if (!fct)
+    return IC_TEST_RSLT_UNSUPPORTED;
+  switch (seq)
+  {
+    case 0:
+      ic_test_reset_internal_vars ();
+      IC_TEST_CHECK ((*fct)(tst, 0, 'a') == EINVAL && errno == EINVAL);
+      IC_TEST_CHECK ((*fct)(NULL, 4, 'a') == EINVAL && errno == EINVAL);
+      IC_TEST_CHECK ((*fct)(tst, 4, 'a') == 0);
+      IC_TEST_CHECK (tst[0] == tst[1] && tst[2] == tst[1]
+		     && tst[1] == 'a');
+      IC_TEST_CHECK ((*fct)(ts2, 3, 'a') == EINVAL && errno == EINVAL);
+      IC_TEST_CHECK ((*fct)(ts3, 5, 0) == 0);
+      IC_TEST_CHECK (ts3[0] == 0);
+      return IC_TEST_RSLT_SUCCESS;
+    default:
+      break;
+  }
+  return IC_TEST_RSLT_FAILED;
+}
+
