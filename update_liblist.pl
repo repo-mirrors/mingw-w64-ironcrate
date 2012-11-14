1b45d401af7b8c8381f0588c5e60a39d7d5b9e92
Jonathan Yong <10walls@gmail.com>
2012-11-14 16:40:25 +0000

    Split out Makefiles and add list builder script
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/experimental/ironcrate@5458 4407c894-4637-0410-b4f5-ada5f102cad1

---
+++ update_liblist.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+# dirs that libironcrate lives in, add them here
+my @src_dir = qw( error except locale memory misc sort threads );
+
+#flags for tests
+my @test_link_flags = qw( CPPFLAGS CFLAGS LDFLAG LDADD );
+
+foreach my $dir (@src_dir){
+  opendir(my $dh, $dir) || die "Can't open $dir\n";
+  my @c_files = grep { m/\.c$/ } readdir($dh);
+  close($dh);
+
+  open(my $fh, '>', "$dir/list.am") || die "Can't write fragment for $dir\n";
+  printf $fh "libironcrate_a_SOURCES += \\\n", $dir;
+  foreach my $file (@c_files){
+    print $fh "$dir/$file";
+  } continue {
+    print $fh " \\" if \$file != \$c_files[-1]; #except last entry
+    print $fh "\n";
+  }
+  close $fh;
+}
+
+my @test_list;
+opendir(my $testsuite, "testsuite") || die "can't open testsuite dir";
+my @tests = grep { !m/testlib/ && !m/^\./ } readdir($testsuite); #exclude testlib
+
+closedir($testsuite);
+foreach my $test(@tests){
+  next if( !-d "testsuite/$test" );
+  opendir(my $test_cases, "testsuite/$test") || die "can't open $test";
+  foreach(grep { m/\.c$/ } readdir($test_cases)){
+    push(@test_list, "testsuite/$test/$_");
+  }
+  closedir($test_cases);
+
+  open(my $test_list_am, '>', 'testsuite/list.am') || die "Can't write fragment for testsuite";
+  print $test_list_am "check_PROGRAMS += \\\n";
+  foreach (@test_list){
+    my $check_PROGRAMS = $_;
+    $check_PROGRAMS =~ s/\.c$//;
+    printf $test_list_am $check_PROGRAMS;
+  } continue {
+    print $test_list_am " \\" if \$_ != \$test_list[-1]; #except last entry
+    print $test_list_am "\n";
+  }
+  print $test_list_am "\n";
+
+  foreach my $flag (@test_link_flags){
+    foreach(@test_list){
+      my $name = $_;
+      $name =~ s/\.c$//;
+      $name =~ s/\//_/g;
+      printf $test_list_am '%s_%s=$(test_%s)'."\n",$name,$flag,lc($flag);
+    }
+  }
+  close($test_list_am);
+}
